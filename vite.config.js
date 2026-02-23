0f3ddfcc4071325d3af685ccb012eea82481b301
danish <danishofcodes@gmail.com>
2026-02-24 01:03:57 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/Wordbox-Game/',
-  plugins: [react()],
+  base :"/Wordbox-Game/",
+  plugins : [react()],
 
 })
